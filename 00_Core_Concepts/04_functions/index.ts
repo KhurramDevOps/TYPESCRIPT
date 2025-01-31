dcbbd27f869b18aa5e68eb70bd6e7b0546f566ca
Khurramshahzad175 <khurramshahzad2440@gmail.com>
2025-01-31 23:23:54 +0500

    added function folder

---
+++ 00_Core_Concepts/04_functions/index.ts
@@ -0,0 +1,26 @@
+// FUNCTIONS IN TYPESCRIPT
+
+// What is Functions?
+// A function is a block of code that can be executed multiple times from different parts of the program
+// It takes some input, processes it and returns some output
+// Functions are reusable blocks of code
+// They can be used to perform a specific task
+// They can be used to simplify the code
+// They can be used to make the code more readable
+
+// Normal Function
+function add(a: number, b: number): number {
+  return a + b;
+}
+console.log(add(50, 80));
+// Arrow Function
+// Arrow function is a shorthand way of writing a function
+// It is a function that is defined using the arrow syntax
+// It is a concise way of writing a function
+
+// Example
+
+const addition = (num1: number, num2: number): number => {
+  return num1 + num2;
+};
+console.log(addition(10, 20));
